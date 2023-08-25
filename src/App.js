fc49bf311d65040b4c2f47cbb0d5d93f8deb8fcc
ndphii <phind76.main@gmail.com>
2023-08-25 12:51:38 +0700

    css new

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./style/App.css"
 const App = () => {
   return (
     <div className="App">
-      <div className="d-flex justify-content-center pt-3">
+      <div className="d-flex justify-content-center pt-1">
         <a href="https://www.facebook.com/ndphii.2002/" target="_blank" rel="noreferrer" className="block-icon col-2"><i className="fa-brands fa-facebook" id="fb"></i><p>Facebook</p></a>
         <a href="https://github.com/ndphii" target="_blank" rel="noreferrer" className="block-icon col-2"><i className="fa-brands fa-github" id="gh"></i><p>Github</p></a>
         <a href="https://www.pinterest.com/ndphii/" target="_blank" rel="noreferrer" className="block-icon col-2"><i className="fa-brands fa-pinterest" id="pt"></i><p>Pinterest</p></a>
